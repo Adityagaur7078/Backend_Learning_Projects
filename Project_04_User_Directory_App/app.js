36599fe832bdffdcda9efe9bc992e72ef5876ee2
Adityagaur7078 <adityagaur7078@gmail.com>
2026-06-25 02:19:40 +0530

    Build user directory app with MongoDB and EJS

---
+++ Project_04_User_Directory_App/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const userModel = require("./models/user");
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/create", async (req, res) => {
+
+    const { name, email, image } = req.body;
+
+    await userModel.create({
+        name,
+        email,
+        image,
+    });
+
+    res.redirect("/read");
+});
+
+app.get("/read", async (req, res) => {
+
+    const allUser = await userModel.find();
+
+    res.render("read", {
+        users: allUser,
+    });
+});
+
+app.get("/delete/:id", async (req, res) => {
+
+    await userModel.findOneAndDelete({
+        _id: req.params.id,
+    });
+
+    res.redirect("/read");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
